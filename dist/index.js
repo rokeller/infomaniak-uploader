e8ae84cc85031a471ec619f977eb9b7c23f0d754
Roger Keller <rokeller@users.noreply.github.com>
2024-03-15 11:28:03 +0100

    Update dist/ after package upgrade.

--- dist/index.js
+++ dist/index.js
@@ -3952,6 +3952,7 @@ var preservedUrlFields = [
   "protocol",
   "query",
   "search",
+  "hash",
 ];
 
 // Create handlers that pass events from native requests
@@ -4385,7 +4386,7 @@ RedirectableRequest.prototype._processResponse = function (response) {
      redirectUrl.protocol !== "https:" ||
      redirectUrl.host !== currentHost &&
      !isSubdomain(redirectUrl.host, currentHost)) {
-    removeMatchingHeaders(/^(?:authorization|cookie)$/i, this._options.headers);
+    removeMatchingHeaders(/^(?:(?:proxy-)?authorization|cookie)$/i, this._options.headers);
   }
 
   // Evaluate the beforeRedirect callback
